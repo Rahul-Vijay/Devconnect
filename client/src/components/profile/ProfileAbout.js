e449b0fe8ecf262a05bac6fa0741313f416a5c3f
Rahul Vijay <vk.rahul318@gmail.com>
2018-06-22 14:32:28 +0530

    added profile display header

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ProfileAbout extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Profile about </h1>
+        <h4> TODO </h4>
+      </div>
+    );
+  }
+}
+
+export default ProfileAbout;
---
+++ client/src/components/profile/ProfileCreds.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ProfileCreds extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Profile Credentials </h1>
+        <h4> TODO </h4>
+      </div>
+    );
+  }
+}
+
+export default ProfileCreds;
